13982cfccff3b576dea65dd2418351ae16b596ee
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-03-20 00:17:54 +0600

    m

--- src/components/DaisyNav/NavBar/NavBar.jsx
+++ src/components/DaisyNav/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 const navData = [
     {
@@ -30,12 +31,22 @@ const navData = [
 ];
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+    const links = navData.map(route => <Link key={route.id} route={route}></Link>)
     return (
-        <nav>
-
-            <ul className=' flex'>
+        <nav className=' flex justify-between mx-10'>
+            <span className=' flex' onClick={() => setOpen(!open)}>
+                { open ? <X className=' md:hidden'></X> :
+                 <Menu className=' md:hidden'></Menu> }
+                <ul className={`md:hidden absolute duration-1000 
+                    ${open ? "top-8": "-top-40"}`}>
+                    {links}
+                </ul>
+                <h3 className=' ml-5'>My Navbar</h3>
+            </span>
+            <ul className=' md:flex hidden'>
                 {
-                    navData.map(route => <Link key={route.id} route={route}></Link>)
+                    links
                 }
             </ul>
 
@@ -52,6 +63,7 @@ const NavBar = () => {
                 <li className=' mr-10'><a href="/about">About</a></li>
                 <li className=' mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
+            <button>Sign In</button>
         </nav>
     );
 };
