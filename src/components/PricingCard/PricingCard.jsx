20b4f3e84c8c0f5f15d7a2bd83af73e7510694d5
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-03-24 19:55:18 +0600

    finish

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -4,19 +4,20 @@ import PricingFeatures from './PricingFeatures';
 const PricingCard = ({ pricing }) => {
     const {name, price, description, features} = pricing;
     return (
-        <div className=' rounded-2xl p-4 shadow-lg bg-amber-500'>
+        <div className=' flex flex-col rounded-2xl p-4 shadow-lg bg-amber-500'>
             {/* card header */}
             <div>
                 <h1 className=' text-7xl'>{name}</h1>
                 <h1 className=' text-3xl'>{price}</h1>
             </div>
-            <div className=' rounded-2xl mt-10 bg-amber-400 p-3'>
+            <div className=' rounded-2xl mt-10 bg-amber-400 p-3 flex-1'>
                 <p>{description}</p>
                 {
                     features.map((feature, index) => <PricingFeatures key={index} features={feature} />)
 
                 }
             </div>
+            <button className="btn btn-active btn-secondary w-full mt-4">Subscribe</button>
         </div>
     );
 };
--- src/components/PricingOptions/PricingOption.jsx
+++ src/components/PricingOptions/PricingOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
 
 const PricingOption = ({ pricingPromise }) => {
 
@@ -7,11 +8,18 @@ const PricingOption = ({ pricingPromise }) => {
     console.log(pricingData)
 
     return (
-        <div className='min-h-screen flex flex-col items-center justify-center px-10 py-10 text-white'>
+        <div className='min-h-screen flex flex-col items-center justify-center px-10 py-10'>
             <h2 className=' text-5xl mb-10'>Get our Membership</h2>
-            <div className=' grid grid-cols-3 gap-8 '>
-                {
+            <div className=' grid md:grid-cols-2 lg:grid-cols-3 gap-8 '>
+                {/* {
                     pricingData.pricingOptions.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+                } */}
+
+                {
+                    pricingData.pricingOptions.map(pricing => <DaisyPricing
+                    key={pricing.id}
+                    pricing={pricing}
+                    ></DaisyPricing>)
                 }
             </div>
         </div>
